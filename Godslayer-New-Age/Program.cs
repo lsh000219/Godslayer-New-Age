ec09b009da9d7627b679c3aca9910883bf6ca48b
lsh000219 <147456856+lsh000219@users.noreply.github.com>
2025-04-21 20:40:32 +0900

    1

--- Godslayer-New-Age/Program.cs
+++ Godslayer-New-Age/Program.cs
@@ -1,23 +1,17 @@
+using Godslayer_New_Age.LJM;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Media;
-using System.IO;
-using Godslayer_New_Age.LJM;
 
 namespace Godslayer_New_Age
 {
     internal class Program
     {
-
-
-
         static void Main(string[] args)
         {
-            //bgm_player bgm_Player = new bgm_player();
-            //bgm_Player.Battle_SinChangSup();
+            
         }
     }
 }
