d229e1a33b276b7a5994c6d209cfaa65aa81e6bd
lsh000219 <147456856+lsh000219@users.noreply.github.com>
2025-04-21 21:06:11 +0900

    0421. 21:06

---
+++ Godslayer-New-Age/kiahn/Shop.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Godslayer_New_Age
+{
+    internal class Shop
+    {
+        static List<ItemData> shopList = Item.itemList;
+
+        public void ShopDisplay()
+        {
+
+        }
+
+        public void BuyItem()
+        {
+
+        }
+
+        public void SellItem()
+        {
+
+        }
+    }
+}
