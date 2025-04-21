a5ef1a0f73bf675cb54066358ba6e216533d34de
lsh000219 <147456856+lsh000219@users.noreply.github.com>
2025-04-21 20:15:01 +0900

    bgmPlayer upload

---
+++ Godslayer-New-Age/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.IO;
+
+namespace Godslayer_New_Age
+{
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            //bgm_player bgm_Player = new bgm_player();
+            //bgm_Player.Battle_SinChangSup();
+        }
+    }
+}
