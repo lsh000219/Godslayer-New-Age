a20be4db7a9e980b9bc659543e91625797331a14
RanKa110 <powerun44@gmail.com>
2025-04-22 14:20:46 +0900

    일부 변경

--- Godslayer-New-Age/Godslayer-New-Age/LJM/Player.cs
+++ Godslayer-New-Age/Godslayer-New-Age/LJM/Player.cs
@@ -6,22 +6,26 @@ using System.Threading.Tasks;
 
 namespace Godslayer_New_Age.LJM
 {
-
-    //    장비 - 무기
-    //    장비 - 방어구
-    //    장비 - 장신구
-
-
     //    플레이어 생성자 - 후에 장비 추가가 된다면 적용 시키기
     internal class Player : Unit
     {
-        public Player(string name, int level, float exp, float hp, float mp, float damage,
-            float defence, int gold, float critRate, float critDmg, float speed, float dodgeRate)
-        : base(name, level, exp, hp, mp, damage, defence, gold, critRate, critDmg, speed, dodgeRate)
+
+        //    장비 - 무기
+        //    장비 - 방어구
+        //    장비 - 장신구
+
+        public List<Skill> PlayerSkills = new List<Skill>();
+
+        public float RequiredExp => (float)(50 * Math.Pow(1.15, Level - 1));
+
+        public Player(string name, int level, float exp, float maxHP, float hp, float maxMP, float mp, float damage,
+            float defence, int gold, float critRate, float critDmg, float speed, float dodgeRate, bool canMove)
+        : base(name, level, exp, maxHP, hp, maxMP, mp, damage, defence, gold, critRate, critDmg, speed, dodgeRate, canMove)
         {
             //    장비 - 무기
             //    장비 - 방어구
             //    장비 - 장신구
+
         }
 
         //    플레이어 생성자 예시. 만약 처음 이름이나 다른 선택지가 있다면 다른곳에서 관리
@@ -31,6 +35,8 @@ namespace Godslayer_New_Age.LJM
             1,
             0f,
             100f,
+            100f,
+            50f,
             50f,
             20f,
             5f,
@@ -38,21 +44,26 @@ namespace Godslayer_New_Age.LJM
             0.1f,
             1.5f,
             1f,
-            0.05f
+            0.05f,
+            true
             //    장비 - 무기
             //    장비 - 방어구
             //    장비 - 장신구
-            //    위에는 초기에는 빈칸이어야 함. 아니면 기초 장비를 끼고 있어야 함
+            //    위에는 초기에는 빈칸이거나 기초 장비를 끼고 있어야 함
             );
 
         //    플레이어 레벨 업
         public void LevelUp()
         {
+            Level++;
+            
             HP += 10f;
             MP += 5f;
             Damage += 0.5f;
             Defence += 1f;
             Console.WriteLine($"{Name}이 레벨업! HP, MP, Damage 증가!");
         }
+
+
     }
 }
--- Godslayer-New-Age/Godslayer-New-Age/LJM/Skill.cs
+++ Godslayer-New-Age/Godslayer-New-Age/LJM/Skill.cs
@@ -8,14 +8,15 @@ namespace Godslayer_New_Age.LJM
 {
     internal class Skill
     {
+        public string SkillName {  get; set; } //    스킬 이름
         public float Cost { get; set; } //    소모량(마나 또는 체력)
         public float Damage { get; set; } //    가하는 데미지 보정
         public float Heal { get; set; } //    만약 치료가 존재한다면 치료 량
         public float EctValue { get; set; } //    만약 다른걸 줄이는것이 존재한다면
 
-
-        public Skill(float cost, float damage, float heal, float cetValue)
+        public Skill(string skillName, float cost, float damage, float heal, float cetValue)
         {
+            SkillName = skillName;
             Cost = cost;
             Damage = damage;
             Heal = heal;
@@ -24,7 +25,8 @@ namespace Godslayer_New_Age.LJM
             //    만약 복수 수치가 필요하다면 몇개 더 추가
         }
 
-        
+        //    스킬 목록
+        public static Skill justHit = new Skill("평타", 0, 5f, 0f, 0f);
 
     }
 }
