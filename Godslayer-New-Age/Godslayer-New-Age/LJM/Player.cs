13c1e17e491adc08c4aa6e87c533c95855af59e0
lsh000219 <147456856+lsh000219@users.noreply.github.com>
2025-04-22 16:03:42 +0900

    LJM

--- Godslayer-New-Age/Godslayer-New-Age/LJM/Player.cs
+++ Godslayer-New-Age/Godslayer-New-Age/LJM/Player.cs
@@ -16,7 +16,9 @@ namespace Godslayer_New_Age.LJM
 
         public List<Skill> PlayerSkills = new List<Skill>();
 
+        //    레벨업에 필요한 경험치로 레벨에 비례해 커짐
         public float RequiredExp => (float)(50 * Math.Pow(1.15, Level - 1));
+        //    50 * (1.15 ^ 레벨 - 1)
 
         public Player(string name, int level, float exp, float maxHP, float hp, float maxMP, float mp, float damage,
             float defence, int gold, float critRate, float critDmg, float speed, float dodgeRate, bool canMove)
@@ -30,26 +32,26 @@ namespace Godslayer_New_Age.LJM
 
         //    플레이어 생성자 예시. 만약 처음 이름이나 다른 선택지가 있다면 다른곳에서 관리
         //    현재는 장비가 장착이 안되어 있음
-        public Player playerUnit = new Player(
-            "플레이어",
-            1,
-            0f,
-            100f,
-            100f,
-            50f,
-            50f,
-            20f,
-            5f,
-            100,
-            0.1f,
-            1.5f,
-            1f,
-            0.05f,
-            true
-            //    장비 - 무기
-            //    장비 - 방어구
-            //    장비 - 장신구
-            //    위에는 초기에는 빈칸이거나 기초 장비를 끼고 있어야 함
+        public static Player playerUnit = new Player(
+            "플레이어",  //    이름
+            1, //    레벨
+            0f, //    경험치
+            100f, //    최대 체력
+            100f, //    현재 체력
+            50f, //    최대 마나
+            50f, //    현재 마나
+            20f, //    공격력
+            5f, //    방어력
+            100, //    소지금
+            0.1f, //    치명타 확률
+            1.5f, //    치명타 대미지
+            1f, //    속도
+            50f, //    회피율
+            true //    행동이 가능한지 파악하는 bool
+                 //    장비 - 무기
+                 //    장비 - 방어구
+                 //    장비 - 장신구
+                 //    위에는 초기에는 빈칸이거나 기초 장비를 끼고 있어야 함
             );
 
         //    플레이어 레벨 업
--- Godslayer-New-Age/Godslayer-New-Age/LJM/Unit.cs
+++ Godslayer-New-Age/Godslayer-New-Age/LJM/Unit.cs
@@ -8,6 +8,7 @@ namespace Godslayer_New_Age.LJM
 {
     internal class Unit
     {
+
         public string Name { get; set; }
         public int Level { get; set; }
         public float EXP { get; set; }
@@ -16,6 +17,7 @@ namespace Godslayer_New_Age.LJM
         public float MaxMP { get; set; }
         public float MP { get; set; }
         public float Damage { get; set; }
+        public int DamageGap { get; set; } = 3;
         public float Defence { get; set; }
         public int Gold { get; set; }
         public float CritRate { get; set; }
@@ -23,6 +25,9 @@ namespace Godslayer_New_Age.LJM
         public float Speed { get; set; }
         public float DodgeRate { get; set; }
         public bool CanMove { get; set; }
+
+
+
 
         //    플레이어 생성자
         public Unit(string name, int level, float exp, float maxHP, float hp, float maxMP, float mp, float damage, float defence,
@@ -62,6 +67,20 @@ namespace Godslayer_New_Age.LJM
             CanMove = canMove;
         }
 
+        //    대미지 갭에 따른 대미지 계산하기
+        public int GetRandomDamage()
+        {
+            //    min은 공격력이 0 이하로 내려가지 않게 해준다
+            int min = Math.Max(0, (int)(Damage - DamageGap));
+            int max = (int)(Damage + DamageGap);
+            return RandomManager.Instance.Next(min, max + 1);
+        }
+
+        //    공격 회피하기 
+        public bool TryDodge()
+        {
+            return RandomManager.Instance.Next(0, 100) < DodgeRate;
+        }
 
     }
 }
