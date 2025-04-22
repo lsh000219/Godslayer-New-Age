b34cd1e526bcffc34c082dab4b7a0803b404e391
lsh000219 <147456856+lsh000219@users.noreply.github.com>
2025-04-22 20:18:18 +0900

    kiahn폴더와 Kiahn폴더가 생기는 것인가...깃허브는 어려워...

--- Godslayer-New-Age/Godslayer-New-Age/kiahn/Inventory.cs
+++ Godslayer-New-Age/Godslayer-New-Age/kiahn/Inventory.cs
@@ -1,49 +1,41 @@
+using Godslayer_New_Age;
+using Godslayer_New_Age.Kiahn;
+using Godslayer_New_Age.LJM;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
+using Utils;
 
 namespace Godslayer_New_Age.Kiahn
 {
-    internal class Inventory
+    internal static class Inventory
     {
         // 인벤토리 리스트
-        static List<ItemData> inventoryList = new List<ItemData>();
+        public static List<ItemData> inventoryList = new List<ItemData>();
         // 장착된 아이템 리스트
-        static List<ItemData> equippedList = new List<ItemData>();
+        public static List<ItemData> equippedList = new List<ItemData>();
 
+        public static List<string> inventoryText = new List<string>() { };
 
-        public void Display()
+        public static void Display()
         {
+            //int i = 0;
+            //inventoryText.Add($"`darkGreen,black`[번호] `darkGray,black`[분류] `white,black`[아이템명] " +
+            //        $"`red,black`[공격력] `cyan,black`[방어력] `white,black`[설명......................] `yellow,black`[가격]");
+            //inventoryText.Add($"`white,black`----------------------------------------------------------------------------------------------------");
+            //foreach (var item in inventoryList)
+            //{
+            //    i++;
+            //    inventoryText.Add($"`darkGreen,black`[{i}] `darkGray,black`[{item.TypeName}] `white,black`{item.Name} `yellow,black`{item.PassiveDesc} " +
+            //        $"`red,black`ATK: {item.Atk} `cyan,black`DEF: {item.Def} `white,black`{item.Desc} `yellow,black`{item.Price} G");
+            //    inventoryText.Add($"`white,black`----------------------------------------------------------------------------------------------------");
+            //}
 
-        }
+            //PrintDB.box1Data = inventoryText;
 
-        // 인벤토리에서 아이템 장착
-        public void Equip(ItemData item)
-        {
-            equippedList.Add(item);
-        }
-
-        // 인벤토리에 아이템 추가
-        public void GetItem(ItemData item)
-        {
-            inventoryList.Add(item);
-            Console.WriteLine($"{item.Name}이(가) 인벤토리에 추가되었습니다.");
-        }
-
-        // 인벤토리에서 아이템 버리기
-        public void DropItem(ItemData item)
-        {
-            if (inventoryList.Contains(item))
-            {
-                inventoryList.Remove(item);
-                Console.WriteLine($"{item.Name}이(가) 인벤토리에서 제거되었습니다.");
-            }
-            else
-            {
-                Console.WriteLine($"{item.Name}은(는) 인벤토리에 없습니다.");
-            }
         }
     }
 }
