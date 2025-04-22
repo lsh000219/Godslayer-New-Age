68ac46c902c62f066a2930898d16e54456047329
lsh000219 <147456856+lsh000219@users.noreply.github.com>
2025-04-22 15:21:05 +0900

    123

--- Godslayer-New-Age/Godslayer-New-Age/Program.cs
+++ Godslayer-New-Age/Godslayer-New-Age/Program.cs
@@ -14,10 +14,10 @@ namespace Godslayer_New_Age
         static void Main(string[] args)
         {
 
-            // bgm_player bgm_Player = new bgm_player();
-            // bgm_Player.Music_Start("Battle_Musk1.wav", false);
-            // bgm_Player.Music_Start("Battle_Musk2.wav", true);
-            // bgm_Player.WaitForExit("Battle_Musk2.wav 루프 재생 중입니다.");
+             bgm_player bgm_Player = new bgm_player();
+             bgm_Player.Music_Start("Battle_Musk1.wav", false);
+             bgm_Player.Music_Start("Battle_Musk2.wav", true);
+             bgm_Player.WaitForExit("Battle_Musk2.wav 루프 재생 중입니다.");
 
             // Console.OutputEncoding = Encoding.UTF8;
             // Console.SetWindowSize(CONSOLE_WIDTH, CONSOLE_HEIGTH);
