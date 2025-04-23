dc592b7fa677e71992eaab12f32fd87361832b53
A7442 <parkjuna1121@naver.com>
2025-04-23 20:20:55 +0900

    feet : 누락된 기능 추가

--- Godslayer-New-Age/Godslayer-New-Age/juna/Dungeon.cs
+++ Godslayer-New-Age/Godslayer-New-Age/juna/Dungeon.cs
@@ -17,17 +17,14 @@ namespace Godslayer_New_Age.juna
 
         private Monster _monster;
         private Battle _battle;
-        public Dungeon(Monster monster)
+        public Dungeon(Monster monster, Battle battle)
         {
             _monster = monster;
-        }
-        public Dungeon(Battle battle)
-        {
             _battle = battle;
         }
 
-        public int dungeontype { get; set; }
-        private bool[] dungeonclear = { false, false, false, false, false };
+        private int dungeontype;
+        private bool[] dungeonclear = { false, false, false, false};
         public int CheckInput(int min, int max)
         {
             while (true)
@@ -45,23 +42,68 @@ namespace Godslayer_New_Age.juna
         public void SelectDungeon(int min, int max)
         {
             Console.WriteLine("가고싶은 장소를 선택해 주세요");
-            Console.WriteLine("1. 단풍향이 피어나는 곳   2. 강아지와 우주선이 오르락내리락하는 곳  3. 협곡  0. 나가기");
+            Console.Write("1. 단풍향이 피어나는 곳");
+            if (dungeonclear[0] == true)
+            {
+                Console.Write("(Clear)");
+            }
+            else
+            {
+                Console.Write("       ");
+            }
+            Console.Write("2. 강아지와 우주선이 오르락내리락하는 곳");
+            if (dungeonclear[1] == true)
+            {
+                Console.Write("(Clear)");
+            }
+            else
+            {
+                Console.Write("       ");
+            }
+            Console.Write("3. 협곡");
+            if (dungeonclear[2] == true)
+            {
+                Console.Write("(Clear)");
+            }
+            else
+            {
+                Console.Write("       ");
+            }
+            if (dungeonclear[0] == true && dungeonclear[1] == true && dungeonclear[2] == true)
+            {
+                Console.Write("4. 이탈리아어가 들리는 곳");
+                if (dungeonclear[3] == true)
+                {
+                    Console.Write("(Clear)");
+                }
+                else
+                {
+                    Console.Write("       ");
+                }
+            }
+            Console.Write("0. 나가기");
+
             dungeontype = CheckInput(0, 4);
             switch (dungeontype)
             {
                 case 1:
-                    Maple();
+                    Console.WriteLine("본섭의 눈물을 듣고 신이 되기로 결정한 신창섭");
+                    Console.WriteLine("리슝좍 10만명을 척결하고 신이 된 그는 이번에는 쌀숭이들 정상화시키려고 한다");
+                    Console.WriteLine("당신과 얼마 안남은 동료들의 생계를 위해 그를 죽여야만 한다... ");
                     AddMonster(1);
                     Dungeon_Select(1);
                     break;
                 case 2:
-                    Coin();
-                    AddMonster(1);
+                    Console.WriteLine("화성과 코인에 의한 여러 밈으로 인해 신이된 자 일론 머스크");
+                    Console.WriteLine("여러분의 주식을 사주는 개미들이 코인을 하러 떠나는 것을 막기 위해 그를 죽여야만 한다");
+                    AddMonster(2);
                     Dungeon_Select(2);
                     break;
                 case 3:
-                    Lol();
-                    AddMonster(1);
+                    Console.WriteLine("리그오브 레전드 게임에서 전무후무한 대기록을 세우며 '롤의 신'이라고 칭송받는 자 페이커");
+                    Console.WriteLine("롤의 신을 넘어 E-sports의 신이 되어 당신의 커리어를 위협한다");
+                    Console.WriteLine("우승이 없는 커리어를 만들지 않으려면 그를 죽여야한다");
+                    AddMonster(3);
                     Dungeon_Select(3);
                     break;
                 case 4:
@@ -70,25 +112,6 @@ namespace Godslayer_New_Age.juna
                 case 0:
                     break;
             }
-        }
-
-        public void Maple()
-        {
-            Console.WriteLine("본섭의 눈물을 듣고 신이 되기로 결정한 신창섭");
-            Console.WriteLine("리슝좍 10만명을 척결하고 신이 된 그는 이번에는 쌀숭이들 정상화시키려고 한다");
-            Console.WriteLine("당신과 얼마 안남은 동료들의 생계를 위해 그를 죽여야만 한다... ");
-        }
-
-        public void Coin()
-        {
-            Console.WriteLine("화성과 코인에 의한 여러 밈으로 인해 신이된 자 일론 머스크");
-            Console.WriteLine("여러분의 주식을 사주는 개미들이 코인을 하러 떠나는 것을 막기 위해 그를 죽여야만 한다");
-        }
-        public void Lol()
-        {
-            Console.WriteLine("리그오브 레전드 게임에서 전무후무한 대기록을 세우며 '롤의 신'이라고 칭송받는 자 페이커");
-            Console.WriteLine("롤의 신을 넘어 E-sports의 신이 되어 당신의 커리어를 위협한다");
-            Console.WriteLine("우승이 없는 커리어를 만들지 않으려면 그를 죽여야한다");
         }
         public void Dungeon_Select(int num)
         {
@@ -102,18 +125,26 @@ namespace Godslayer_New_Age.juna
                 {
                     case 1:
                         ShowMonster(room);
+                        if (dungeonclear[num] == true && (room == 4 || room == 9))
+                        {
+                            room++;
+                        }
                         room++;
                         break;
                     case 0:
                         break;
                 }
-                if(playerselect ==0 || Player.Instance.HP == 0)
+                if (playerselect == 0 || Player.Instance.HP == 0)
                 {
-                    if(Player.Instance.HP == 0)
+                    if (Player.Instance.HP == 0)
                     {
                         Player.Instance.HP = 1.0f;
                     }
                     break;
+                }
+                if (room==11)
+                {
+                    dungeonclear[num-1] = true;
                 }
             }
         }
@@ -123,16 +154,20 @@ namespace Godslayer_New_Age.juna
             {
                 case 1:
                 case 2:
+                case 3:
                 case 4:
-                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
                     int randnum1 = random.Next(0, 2);
                     int randnum2 = random.Next(0, 2);
                     _battle.StartBattle(monsters[randnum1], monsters[randnum2]);
                     break;
-                case 3:
+                case 5:
                     _battle.StartBattle(monsters[2]);
                     break;
-                case 6:
+                case 10:
                     _battle.StartBattle(monsters[3]);
                     break;
                 default:
