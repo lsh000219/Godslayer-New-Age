62a9b4ddfcafecab1b6b1164b2945cfa4182008f
lsh000219 <147456856+lsh000219@users.noreply.github.com>
2025-04-23 21:21:53 +0900

    lsh - finally_Awake 추가

--- Godslayer-New-Age/Godslayer-New-Age/Program.cs
+++ Godslayer-New-Age/Godslayer-New-Age/Program.cs
@@ -19,7 +19,7 @@ namespace Godslayer_New_Age
             //bgm test
             /*
             bgm_player bgm_Player = new bgm_player();
-            bgm_Player.Music_Start("Victory.wav", false);
+            bgm_Player.Music_Start("Finally_Awake.wav", false);
             bgm_Player.Music_Start("Battle_Musk2.wav", true);
             Console.ReadLine();
             bgm_Player.Music_Exit();
