7c94bdd91930660fcb896493157052f1b8634577
ragura635 <ragura635@gmail.com>
2025-04-25 23:14:42 +0900

    fix: .....

--- Godslayer-New-Age/Godslayer-New-Age/Data/Buff.cs
+++ Godslayer-New-Age/Godslayer-New-Age/Data/Buff.cs
@@ -40,8 +40,8 @@ public class Buff
         _RemainingTurn = duration;
     }
 
-    public Buff Poison = new Buff("독", BuffType.DamageOnTurn,
-        new Effect(EffectType.DrainHP, 10), 5);
+    //public Buff Poison = new Buff("독", BuffType.DamageOnTurn,
+    //    new Effect(EffectType.DrainHP, 10), 5);
 
 
     //    실질적인 버프의 효과를 주는 곳
