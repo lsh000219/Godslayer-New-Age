2e653d0dd0257c60f112d341dfb700ca095882d2
RanKa110 <powerun44@gmail.com>
2025-04-25 17:21:33 +0900

    스킬 몇개 제작

--- Godslayer-New-Age/Godslayer-New-Age/LJM/Skill.cs
+++ Godslayer-New-Age/Godslayer-New-Age/LJM/Skill.cs
@@ -1,6 +1,7 @@
 using Godslayer_New_Age.LJM;
 using System.Collections.Generic;
 using System;
+using System.Runtime.CompilerServices;
 
 public class Skill
 {
@@ -20,7 +21,8 @@ public class Skill
     }
 
 
-    //    ===== 스킬 만드는 곳! ===== //
+    //    ================================ 스킬 만드는 곳! ================================ //
+    //    ================================ 여기는 예시로 만든 것들 ================================
     public static Skill justHit = new Skill("때리기", 0, 100, 
         new List<Effect> 
         {
@@ -49,6 +51,61 @@ public class Skill
             new Effect(EffectType.DrainHP, 10), 3)
             //    3턴 동안 10의 방어무시 대미지 가하기
         });
+
+
+    //     ================================ 여기서부턴 노션에 올라왔던 내용들 ================================
+    //    쌀숭이==================
+    public static Skill HuntingInPlace = new Skill("재자리 사냥", 0, 100,
+        new List<Effect>
+        {
+            //    사용 시 대상과 시전자를 입력해야 함(공격력 비례 대미지)
+            new Effect(EffectType.DamageWithAtkScale, 1f)
+        });
+
+    public static Skill PostInInven = new Skill("인벤에 글쓰기", 10, 100,
+        new List<Effect>
+        {
+            //    사용 시 대상과 시전자를 입력해야 함(공격력 비례 대미지)
+            new Effect(EffectType.DamageWithAtkScale, 1.2f)
+        });
+
+    public static Skill WWE = new Skill("WWE", 20, 100,
+       new List<Effect>
+       {
+            //    사용 시 대상과 시전자를 입력해야 함(공격력 비례 대미지)
+            new Effect(EffectType.DamageWithAtkScale, 0.8f)
+           //    그럼 타겟도 List로 받아와야 하나?
+           //    일단 이건 단일 타겟
+       });
+
+    /*
+    public static Skill Origin = new Skill("오리진", 20, 100,
+       new List<Effect>
+       {
+            //    사용 시 대상과 시전자를 입력해야 함(공격력 비례 대미지)
+            new Effect(EffectType.DamageWithAtkScale, 3f),
+       },
+       new List<Buff>
+       {
+           new Buff("스턴", BuffType.Stun, )
+       });
+    */
+    //    스턴은 따로 되돌리는 로직을 짜야할 듯 싶다..
+
+    //    CEO==================
+    public static Skill AllInStock = new Skill("풀매수", 10, 30,
+        new List<Effect>
+        {
+            
+            //    사용 시 대상과 시전자를 입력해야 함(공격력 비례 대미지)
+            new Effect(EffectType.DamageWithAtkScale, 1.2f)
+            //    흠... 확률에 실패한다면 ~가 나오기가 여기서 구현하기는 힘들구나
+            //    전투쪽이라면 할 수 있겠지?
+        });
+
+
+    //    strong hand
+    //    FOMO
 
     ///////////////////////////////////////////////////////////////////////////
 
