5aa56c7c7249e3c4d75c434b0f80c33e27bbc0ae
A7442 <parkjuna1121@naver.com>
2025-04-27 18:35:04 +0900

    feet : startscene 추가

--- Godslayer-New-Age/Godslayer-New-Age/Scenes/StartScene.cs
+++ Godslayer-New-Age/Godslayer-New-Age/Scenes/StartScene.cs
@@ -27,13 +27,29 @@ internal class StartScene : IScene
     public StartScene()
     {
         box1Text[0] = new List<string>()
-            {
-                "스토리",
-                "  어쩌구",
-                "    저쩌구",
-                "",
-                "보스는 `Yellow,red`신창섭`null,black`, `white,darkblue`일론머스크`null,black`, `red,white`페이커`null,black`",
-            };
+        {
+            "",
+            "신....종교의 대상으로 초인간적, 초자연적 위력을 가지고 인간에게 화와 복을 내린다고 믿어지는 존재",
+            "주로 인간보다 우월하여 전지전능에 가깝거나 그러한 힘을 지닌 존재를 의미한다.",
+            "",
+            "",
+            "이런 신을 죽인 자들이 있었으니... 그들이 `red,black`God Slayer(신살자)`gray,black`이다.",
+            "처음으로 불을 피우는 법을 밝혀내 불이라는 신을 죽인 고대의 누군가,",
+            "가장 유명한 신(예수)을 죽인 병사 롱기누스, ",
+            "그리고 가장 현대의 신살자이며 현대까지 남아있는 모든 신을 죽인 철학자 니케",
+            "",
+            "",
+            "이로써 완전한 인간의 시대를 맞이한 인류는 우주 여행, AI와 같은 고도의 과학 발전을 이룩한다",
+            "하지만 신은 현대에도 다시 태어나는 법",
+            "그 신들은  `Yellow,red`신창섭`null,black`, `white,darkblue`일론머스크`null,black`, `red,white`페이커`null,black`",
+            "",
+            "",
+            "",
+            "",
+            "그리고 당신은 현대의 신들을 죽일 새로운 시대의 `red,black`God Slayer(신살자)`gray,black`이다...",
+
+
+        };
 
         box3Text[0] = new List<string>()
             {
